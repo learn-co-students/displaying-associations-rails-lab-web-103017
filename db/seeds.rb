c5f8496f718c1fbc69133a08f81a0c54ecb9e538
dansbands <dansbands@hotmail.com>
2017-11-30 12:48:14 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artists = Artist.create([{name: 'Dan'}, {name: 'Gerry'}])
+songs = Song.create([{title: 'Gravelyard', artist_id: 1}, {title: 'Viaduct', artist_id: 2}, {title: 'The Weight', artist_id: 1}])
